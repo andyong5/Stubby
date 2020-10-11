85353251fef3dd501ee13bb770b7bd24d622de2e
Andy Nguyen <acnguye@ucdavis.edu>
2020-10-11 12:10:48 -0700

    test

--- stubby/routes.py
+++ stubby/routes.py
@@ -92,17 +92,17 @@ def logout():
     return redirect(url_for('front'))
 
 
-def save_picture(form_picture):
-    random_hex = secrets.token_hex(8)
-    _, f_ext = os.path.splitext(form_picture.file_name)
-    picture_fn = random_hex + f_ext
-    picture_path = os.path.join(
-        app.root_path, '/static/profile_pic', picture_fn)
-    output_size(125, 125)
-    i = Image.open(form_picture)
-    i.thumbnail(output_size)
-    i.save(picture_path)
-    return picture_fn
+# def save_picture(form_picture):
+#     random_hex = secrets.token_hex(8)
+#     _, f_ext = os.path.splitext(form_picture.file_name)
+#     picture_fn = random_hex + f_ext
+#     picture_path = os.path.join(
+#         app.root_path, '/static/profile_pic', picture_fn)
+#     output_size(125, 125)
+#     i = Image.open(form_picture)
+#     i.thumbnail(output_size)
+#     i.save(picture_path)
+#     return picture_fn
 
 
 @app.route('/account', methods=["GET", "POST"])
